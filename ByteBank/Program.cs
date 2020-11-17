b5d06c43f40abc7c128d255d58ddbca6784b1ea2
Wesley <wesley.contato@hotmail.com>
2020-11-17 19:44:50 -0300

    Blocos Try Catch

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,7 +6,17 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Metodo();
+            try
+            {
+                Metodo();
+
+            }
+            catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+
             Console.ReadLine();
         }
         //Teste com a cadeia de chamada:
@@ -18,13 +28,25 @@ namespace ByteBank
 
         private static void TestaDivisao(int divisor)
         {
-            int resultado = Dividir(10, divisor);
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
 
-            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possível fazer uma divisão por 0!");
+            }
         }
 
         private static int Dividir(int numero, int divisor)
         {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+
             return numero / divisor;
         }
     }
