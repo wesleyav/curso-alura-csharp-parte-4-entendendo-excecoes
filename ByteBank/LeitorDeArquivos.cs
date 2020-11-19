a5d5fce484872338eb238fbc2dfada6ad0bdd054
Wesley <wesley.contato@hotmail.com>
2020-11-18 22:27:42 -0300

    Liberando recursos

--- ByteBank/LeitorDeArquivos.cs
+++ ByteBank/LeitorDeArquivos.cs
@@ -9,27 +9,29 @@ namespace ByteBank
 {
     class LeitorDeArquivos
     {
-        public class LeitorDeArquivo
+        public class LeitorDeArquivo : IDisposable
         {
             public string Arquivo { get; }
 
             public LeitorDeArquivo(string arquivo)
             {
                 Arquivo = arquivo;
-                throw new FileNotFoundException();
+
+                //throw new FileNotFoundException();
                 Console.WriteLine("Abrindo arquivo: " + arquivo);
             }
 
             public string LerProximaLinha()
             {
                 Console.WriteLine("Lendo linha...");
-                //throw new IOException();
+                throw new IOException();
                 return "Linha do arquivo";
             }
 
-            public void Fechar()
+            public void Dispose()
             {
                 Console.WriteLine("Fechando arquivo.");
+
             }
         }
     }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -12,8 +12,14 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-
-            CarregarContas();
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
 
             Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
@@ -21,26 +27,32 @@ namespace ByteBank
 
         private static void CarregarContas()
         {
-            LeitorDeArquivo leitor = null;
-            try
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
             {
-                leitor = new LeitorDeArquivo("contas.txt");
-                leitor.LerProximaLinha();
-                leitor.LerProximaLinha();
+                // IDisposible
                 leitor.LerProximaLinha();
             }
-            catch (IOException)
-            {
 
-                Console.WriteLine("Exceção do tipo IOException capturada e tratada!.");
-            }
-            finally
-            {
-                if (leitor != null)
-                {
-                    leitor.Fechar();
-                }
-            }
+
+
+            // ---------------
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally.");
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
         }
 
         private static void TestaInnerException()
